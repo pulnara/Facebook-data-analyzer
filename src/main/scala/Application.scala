41a06f9177445e6e85763dc545f3103029228d21
Karolina <noreply>
2018-06-10 13:53:27 +0200

    Added (sorted)CountWordsInConversation

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,7 +1,11 @@
 object Application {
   def main(args: Array[String]): Unit = {
 //    println("Kanapka!!!")
-    val reactionsCounter = new ReactionsCounter("/home/aga/Pobrane/FBData/likes_and_reactions/posts_and_comments.json")
+    val reactionsCounter = new ReactionsCounter("D:/WIEiT/Scala/facebook-dane/likes_and_reactions/posts_and_comments.json")
     reactionsCounter.print()
+    val wordsInConversationParser = new WordsInConversationParser("D:/WIEiT/Scala/facebook-dane/messages/AgnieszkaPulnar_ca96c8a69a/message.json")
+    wordsInConversationParser.parse()
+    ///home/aga/Pobrane/FBData/likes_and_reactions/posts_and_comments.json
+
   }
 }
