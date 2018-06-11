a20224865ad8a61da89886a222dfacb89412ff17
pulnara <magnes.agnieszka@gmail.com>
2018-06-11 15:55:24 +0200

    Add basic directory handling

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,19 +1,34 @@
+import java.io.File
+import java.security.InvalidParameterException
+
 object Application {
   def main(args: Array[String]): Unit = {
-//    println("Kanapka!!!")
-    val reactionsCounter = new ReactionsCounter("D:/WIEiT/Scala/facebook-dane/likes_and_reactions/posts_and_comments.json")
-    reactionsCounter.print()
-    val wordsInConversationParser = new WordsInConversationParser("D:/WIEiT/Scala/facebook-dane/messages/AgnieszkaPulnar_ca96c8a69a/message.json")
-    val topNSelector = new TopNSelector(10, wordsInConversationParser)
-    topNSelector.print()
-    topNSelector.printLongerWords()
+    if (args.length != 1) {
+      println("Proper argument: <PATH> to your FB data's directory.")
+      System.exit(1)
+    }
+//    println(args{0})
+    val path = args{0}
+    val file = new File(path)
+    try {
+      if (file.exists() && file.isDirectory) {
+        val reactionsCounter = new ReactionsCounter(file + "/likes_and_reactions/posts_and_comments.json")
+        reactionsCounter.print()
+        val wordsInConversationParser = new WordsInConversationParser(file + "/messages/agnieszkapulnar_2f35bd08c5/message.json")
+        val topNSelector = new TopNSelector(10, wordsInConversationParser)
+        topNSelector.print()
+        topNSelector.printLongerWords()
+      }
+      else {
+        throw new InvalidParameterException("Invalid path given.")
+      }
+    } catch {
+      case e => println(e.getMessage)
+    }
 
-    val wordsInConversationsParser = new WordsInConversationsParser("D:/WIEiT/Scala/facebook-dane/messages/")
-    wordsInConversationsParser.parse()
-    val topNSelector2 = new TopNSelector(10,  wordsInConversationsParser)
-    topNSelector2.print()
-    topNSelector2.printLongerWords()
     ///home/aga/Pobrane/FBData/likes_and_reactions/posts_and_comments.json
+    //"D:/WIEiT/Scala/facebook-dane/likes_and_reactions/posts_and_comments.json"
+    // "D:/WIEiT/Scala/facebook-dane/messages/AgnieszkaPulnar_ca96c8a69a/message.json"
 
   }
 }
