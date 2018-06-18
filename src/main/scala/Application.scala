4253c7fcb3a05eb08f1bbf55be96b5c2427c5d17
pulnara <magnes.agnieszka@gmail.com>
2018-06-18 04:01:35 +0200

    Improve directory handling

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -2,12 +2,21 @@ import java.io.File
 import java.security.InvalidParameterException
 
 object Application {
+  def getPersonsMessagesPath(path : String, name : String) : String = {
+    val file = new File(path + "/messages/")
+    val result = file.listFiles().filter(_.isDirectory).map(_.getPath).toList.filter(f => f.contains(name.toLowerCase))
+      if (result.nonEmpty) {
+        result.head + "/message.json"
+      } else {
+        throw new InvalidParameterException("Invalid person name.")
+      }
+  }
+
   def main(args: Array[String]): Unit = {
     if (args.length != 1) {
       println("Proper argument: <PATH> to your FB data's directory.")
       System.exit(1)
     }
-//    println(args{0})
     println("Your Facebook Data Analysis:\n")
     val path = args{0}
     val file = new File(path)
@@ -15,18 +24,29 @@ object Application {
       if (file.exists() && file.isDirectory) {
         val reactionsCounter = new ReactionsCounter(file + "/likes_and_reactions/posts_and_comments.json")
         reactionsCounter.print()
-        val wordsInConversationParser = new WordsInConversationParser(file + "/messages/agnieszkapulnar_2f35bd08c5/message.json")
+
+//        println("Type name of person:")
+//        val who = scala.io.StdIn.readLine().replaceAll(" ", "")
+        val who = "AgnieszkaPulnar"
+        val messagesPath = getPersonsMessagesPath(path, who)
+        val wordsInConversationParser = new WordsInConversationParser(messagesPath)
         val topNSelector = new TopNSelector(10, wordsInConversationParser)
         topNSelector.print()
         topNSelector.printLongerWords()
-        val messagesInConversationCounter = new MessagesInConversationCounter(file + "/messages/agnieszkapulnar_2f35bd08c5/message.json")
+
+        val messagesInConversationCounter = new MessagesInConversationCounter(messagesPath)
         messagesInConversationCounter.print()
+
         val messagesInConversationsParser = new MessagesInConversationsParser(file + "/messages")
         val topNSelector2 = new TopNSelector(10, messagesInConversationsParser)
         println("Top messages in conversations:")
         topNSelector2.print()
+
         val commentsCounter = new CommentsCounter(file + "/comments/comments.json")
         commentsCounter.print()
+
+        val friendsNumberAnalyzer = new FriendsNumberAnalyzer(file + "/friends/friends.json")
+        friendsNumberAnalyzer.print()
 
       }
       else {
@@ -35,13 +55,5 @@ object Application {
     } catch {
       case e: InvalidParameterException => println(e.getMessage)
     }
-
-    ///home/aga/Pobrane/FBData/likes_and_reactions/posts_and_comments.json
-    //"/messages/agnieszkapulnar_2f35bd08c5/message.json"
-
-    //"D:/WIEiT/Scala/facebook-dane/likes_and_reactions/posts_and_comments.json"
-    //"D:/WIEiT/Scala/facebook-dane/messages/AgnieszkaPulnar_ca96c8a69a/message.json"
-
-
   }
 }
